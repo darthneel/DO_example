97ed098567d9381a88aad9f5fe06e7e1ae83a9f5
neelp22 <neelp22@gmail.com>
2014-12-17 10:41:22 -0500

    fixing typo in conn file

--- db/connection.rb
+++ db/connection.rb
@@ -1,5 +1,8 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection('postgresql://127.0.0.1/do_test')
+ActiveRecord::Base.establish_connection('postgresql://' + ENV['DB_USERNAME'] + ':' + ENV['DB_PASSWORD'] + '104.236.99.167')
+
+# postgres://vfbqiffkdqbayx:anCTgwZ8213KW1sUfjDn0hJNbH@ec2-54-197-238-242.compute-1.amazonaws.com:5432/d6l6k3pflim1p0
+
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
