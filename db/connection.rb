ffbc178fc15c13bdd279111239337a62042e1e8f
neelp22 <neelp22@gmail.com>
2014-12-17 10:29:26 -0500

    fixing typo in conn file

--- db/connection.rb
+++ db/connection.rb
@@ -1,5 +1,5 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(postgresql://127.0.0.1/do_test)
+ActiveRecord::Base.establish_connection('postgresql://127.0.0.1/do_test')
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
